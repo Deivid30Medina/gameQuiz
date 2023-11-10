eb10a593be3aab8399a48546f7e7c65297f8d7d0
Deivid30Medina <144353360+Deivid30Medina@users.noreply.github.com>
2023-11-10 15:03:48 -0500

    Corrigiendo bug palabras vacias

--- js/banco-de-preguntas.js
+++ js/banco-de-preguntas.js
@@ -1,17 +1,5 @@
 const opcionRespuestaCorrecta = "Esta es correcta";
-let opcionIncorrecta = [
-    "Esta no es 0"
-    ,"Esta no es 1"
-    ,"Esta no es 2"
-    ,"Esta no es 3"
-    ,"Esta no es 4"
-    ,"Esta no es 5"
-    ,"Esta no es 6"
-    ,"Esta no es 7"
-    ,"Esta no es 8"
-    ,"Esta no es 9"
-    ,"Esta no es 10"
-];
+let opcionIncorrecta = [];
 
 function inicializarVariables(){
     opcionIncorrecta = [
