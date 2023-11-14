ce20dd14128d47d62fba952d5b8125e46167e7b4
Deivid30Medina <144353360+Deivid30Medina@users.noreply.github.com>
2023-11-14 10:10:29 -0500

    Se ajusto el temporizador para que quede rojo al finalziar el tiempo

--- js/temporizador.js
+++ js/temporizador.js
@@ -1,7 +1,8 @@
 let tiempoTotal = 10;
 let tiempoRestante = tiempoTotal;
-let temporizadorElemento = document.getElementById('timer');
+let temporizadorElemento ;
 let temporizadorId; // Almacena el ID del temporizador actual
+let contenedorTemporizador = document.getElementById('idContainerTemporizador');
 
 function actualizarTemporizador() {
   let minutos = Math.floor(tiempoRestante / 60);
@@ -16,9 +17,51 @@ function actualizarTemporizador() {
 function startTemporizador() {
   // Cancelar el temporizador existente antes de iniciar uno nuevo
   clearTimeout(temporizadorId);
-
+  eliminarTemporizadorHtml();
+  crearTemporizadorHtml();
+  temporizadorElemento = document.getElementById('timer');
   tiempoRestante = tiempoTotal;
   comenzarTemporizador();
+}
+
+function eliminarTemporizadorHtml(){
+  let elementoTemporizador = document.getElementById('idTemproizador');
+  if (elementoTemporizador) {
+    elementoTemporizador.remove();
+  }
+}
+
+function eliminarAnimacionCss(){
+  console.log("Entro")
+  let divWrapper = document.getElementById('idWrapper');
+  let divSpinner = document.getElementById('idSpinner');
+  let divFiller = document.getElementById('idFiller');
+  let divMask = document.getElementById('idMask');
+  divSpinner.style.animation = "none";
+  divFiller.style.animation = "none";
+  divMask.style.animation = "none";
+  divFiller.style.opacity = 1;
+  divMask.style.opacity = 1;
+  divMask.style.zIndex = 0;
+}
+
+function crearTemporizadorHtml(){
+  let nuevoTemporizador = document.createElement('div');
+  nuevoTemporizador.id = 'idTemproizador';
+  nuevoTemporizador.className = 'classTemporizador';
+
+  // Agrega aquí el contenido HTML para el temporizador
+  nuevoTemporizador.innerHTML = `
+    <div id="idWrapper" class="wrapper">
+      <div id="idSpinner" class="pie spinner"></div>
+      <div id="idFiller" class="pie filler"></div>
+      <div id="idMask" class="mask"></div>
+    </div>
+    <p id="timer">00:00</p>
+  `;
+
+  // Adjunta el nuevo temporizador al contenedor
+  contenedorTemporizador.appendChild(nuevoTemporizador);
 }
 
 function comenzarTemporizador() {
@@ -27,8 +70,9 @@ function comenzarTemporizador() {
     actualizarTemporizador();
     temporizadorId = setTimeout(comenzarTemporizador, 1000);
   } else {
+    eliminarAnimacionCss();
     // Si el tiempo llega a cero, reiniciar el temporizador
-    startTemporizador(); // Reiniciar el temporizador
+    //startTemporizador(); // Reiniciar el temporizador
   }
 }
 
